9ca0641cc6dbc30584ae776bff8dc11453c0a820
ChanduPingili <126405951+ChanduPingili@users.noreply.github.com>
2023-11-07 19:50:29 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,21 @@
+
+function add(){
+  let a = document.getElementById("value1").value*1
+  let b = document.getElementById("value2").value*1
+  document.getElementById("result").innerHTML=a+b;
+}
+function sub(){
+  let a = document.getElementById("value1").value*1
+  let b = document.getElementById("value2").value*1
+  document.getElementById("result").innerHTML=a-b;
+}
+function mul(){
+  let a = document.getElementById("value1").value*1
+  let b = document.getElementById("value2").value*1
+  document.getElementById("result").innerHTML=a*b;
+}
+function div(){
+  let a = document.getElementById("value1").value*1
+  let b = document.getElementById("value2").value*1
+  document.getElementById("result").innerHTML=a/b;
+}
